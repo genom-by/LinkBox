6b0274c5dfa49ba30bca5e71fad81824d6d837a6
genom-by <brath@mail.ru>
2013-08-16 21:27:10 +0300

    db added

---
+++ cgi/settings.inc.php
@@ -0,0 +1,7 @@
+<?php
+define(ISSQLITE, 'true');
+define(DBPATHSQLITE,'linkbox2.db2');
+define(DSNSQLITE,'sqlite2:');
+define(PAGING_NUM_PER_PAGE,'10');
+
+define(SITE_ROOT, 'tt/atwebpages/LinkBox');
