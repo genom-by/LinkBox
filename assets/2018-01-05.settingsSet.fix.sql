5f94a4d810a3f440dc878300dace95b85869309b
genom-by <brath@mail.ru>
2018-01-05 21:17:54 +0300

    settings db table inserted

---
+++ assets/2018-01-05.settingsSet.fix.sql
@@ -0,0 +1,18 @@
+INSERT INTO user_settings(uid, setid, `value` )
+VALUES(1, 1, 'en');
+INSERT INTO user_settings(uid, setid, `value` )
+VALUES(1, 2, 'pillbox');
+INSERT INTO user_settings(uid, setid, `value` )
+VALUES(1, 3, 'n');
+INSERT INTO user_settings(uid, setid, `value` )
+VALUES(1, 4, 'n');
+INSERT INTO user_settings(uid, setid, `value` )
+VALUES(4, 1, 'en');
+INSERT INTO user_settings(uid, setid, `value` )
+VALUES(4, 2, 'pillbox');
+INSERT INTO user_settings(uid, setid, `value` )
+VALUES(4, 3, 'n');
+INSERT INTO user_settings(uid, setid, `value` )
+VALUES(4, 4, 'n');
+INSERT INTO user_settings(uid, setid, `value` )
+VALUES(4, 5, 25);
